0eb100ad14a350246d0c2eac6d0dd215b7aa162e
ligun123 <li376438624@gmail.com>
2012-07-31 14:13:15 +0800

    finished

--- AbstractFactory/main.cpp
+++ AbstractFactory/main.cpp
@@ -7,12 +7,45 @@
 //
 
 #include <iostream>
+#include "UIFactory.h"
+
+
+/*
+ In a project , For example , You want to create UI of Iphone or Ipad, Just change  Factory.CreateMacWindow() || Factory.CreateLinuxWindow()  is OK
+ */
 
 int main(int argc, const char * argv[])
 {
-
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    UIFactory Factory = UIFactory();
+    printf("*********test    Mac     UI*****************\n");
+    //test mac UI
+    Window *macWindow = Factory.CreateMacWindow();
+    Bar *macBar = macWindow->CreateBar();
+    Button *macButton = macWindow->CreateButton();
+    View *macView = macWindow->CreateView();
+    macBar->BarAction();
+    macButton->ButtonAction();
+    macView->ViewAction();
+    delete macBar;
+    delete macButton;
+    delete macView;
+    delete macWindow;
+    
+    printf("*********test    linux   UI*****************\n");
+    //test linux UI
+    Window *linuxWindow = Factory.CreateLinuxWindow();
+    Bar *linuxBar = linuxWindow->CreateBar();
+    Button *linuxButton = linuxWindow->CreateButton();
+    View *linuxView = linuxWindow->CreateView();
+    linuxBar->BarAction();
+    linuxButton->ButtonAction();
+    linuxView->ViewAction();
+    delete linuxBar;
+    delete linuxButton;
+    delete linuxView;
+    delete linuxWindow;
+    
     return 0;
 }
-
